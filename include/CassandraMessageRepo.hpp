f75eecb34193888824e989e203c7e6b79881d84a
manoj4617 <manojsainikwod4617@gmail.com>
2025-08-29 20:19:58 +0530

    feat: implement Cassandra database initialization and query execution

--- include/CassandraMessageRepo.hpp
+++ include/CassandraMessageRepo.hpp
@@ -5,6 +5,20 @@
 #include <cassandra.h>
 #include <memory>
 
+static constexpr const char* CREATE_KEYSPACE_QUERY =
+        "CREATE KEYSPACE IF NOT EXISTS chat_app "
+        "WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};";
+
+static constexpr const char* CREATE_MESSAGES_TABLE_QUERY =
+        "CREATE TABLE IF NOT EXISTS chat_app.messages ("
+        "    barrack_id text,"
+        "    message_id timeuuid,"
+        "    sender_id text,"
+        "    content text,"
+        "    timestamp timestamp,"
+        "    PRIMARY KEY (barrack_id, message_id)"
+        ") WITH CLUSTERING ORDER BY (message_id DESC);";
+
 struct CassandraConnection {
     CassCluster *cluster = nullptr;
     CassSession *session = nullptr;
@@ -23,12 +37,14 @@ struct CassandraConnection {
 class CassandraMessageRepo : public MessageRepository {
     public:
         explicit CassandraMessageRepo(std::shared_ptr<CassandraConnection> cass_conn);
-
+        ~CassandraMessageRepo();
+        Result<std::monostate> init_database();
         Result<std::monostate> add(const ChatMessage& message) override;
         Result<std::vector<ChatMessage>> get_for_barrack(const std::string& barrack_id, int limit) override;
 
     private:
         std::shared_ptr<CassandraConnection> conn_;
+        Result<std::monostate> execute_simple_query(const char* query);
         const CassPrepared* add_message_prepared_ = nullptr;
         const CassPrepared* get_message_prepared_ = nullptr;
 };
--- src/CassandraMessageRepo.cpp
+++ src/CassandraMessageRepo.cpp
@@ -1,4 +1,7 @@
+#include "Error.hpp"
 #include <CassandraMessageRepo.hpp>
+#include <cassandra.h>
+#include <variant>
 
 CassandraMessageRepo::CassandraMessageRepo(std::shared_ptr<CassandraConnection> cass_conn){
     conn_ = cass_conn;
@@ -9,6 +12,46 @@ CassandraMessageRepo::CassandraMessageRepo(std::shared_ptr<CassandraConnection> 
                   << cass_error_desc(ec) << std::endl;
         throw std::runtime_error("Cassandra connection setup failed!!");
     }
+
+    CassFuture* cass_future = cass_session_connect(conn_->session, conn_->cluster);
+    if(cass_future_error_code(cass_future) == CASS_OK){
+        std::cout << "Successfully connected to Cassandra!!\n";
+        cass_future_free(cass_future);
+    }
+    else {
+        const char* message;
+        size_t message_size;
+        cass_future_error_message(cass_future, &message, &message_size);
+        std::cerr << "Unable to connect to Cassandra: " << std::string(message, message_size) << std::endl;
+        cass_future_free(cass_future);
+    }
+}
+
+CassandraMessageRepo::~CassandraMessageRepo() {
+    if (add_message_prepared_) cass_prepared_free(add_message_prepared_);
+    if (get_message_prepared_) cass_prepared_free(get_message_prepared_);
+}
+
+Result<std::monostate> CassandraMessageRepo::init_database(){
+    std::cout << "Initializing Cassandra Schema..." << std::endl;
+    auto keyspace_res = execute_simple_query(CREATE_KEYSPACE_QUERY);
+    if(std::holds_alternative<Error>(keyspace_res)){
+        std::cerr << "Failed to create keyspace.\n";
+        return keyspace_res;
+    }
+
+    std::cout << "Keyspace 'chat_app' is ready.\n";
+
+    auto table_creation_res = execute_simple_query(CREATE_MESSAGES_TABLE_QUERY);
+    if(std::holds_alternative<Error>(table_creation_res)){
+        std::cerr << "Table creation failed.\n";
+        return table_creation_res;
+    }
+
+    std::cout << "Table 'chat_app.messages' is ready.\n";
+
+    std::cout << "Cassandra schema initialization complete.\n";
+    return Success{};
 }
 
 Result<std::monostate> CassandraMessageRepo::add(const ChatMessage &message) {
@@ -17,4 +60,30 @@ Result<std::monostate> CassandraMessageRepo::add(const ChatMessage &message) {
 
 Result<std::vector<ChatMessage>> CassandraMessageRepo::get_for_barrack(const std::string &barrack_id, int limit) {
 
+}
+
+Result<std::monostate> CassandraMessageRepo::execute_simple_query(const char* query){
+    CassStatement* cass_statement = cass_statement_new(query, 0);
+    cass_statement_set_consistency(cass_statement, CASS_CONSISTENCY_QUORUM);
+
+    CassFuture* cass_future = cass_session_execute(conn_->session, cass_statement);
+    cass_future_wait(cass_future);
+
+    CassError rc = cass_future_error_code(cass_future);
+
+    Result<std::monostate> result;
+    if(rc != CASS_OK){
+         const char* message;
+        size_t message_length;
+        cass_future_error_message(cass_future, &message, &message_length);
+        std::string error_msg = "Cassandra query failed: " + std::string(message, message_length);
+        result = Error{ErrorCode::DATABASE_ERROR, error_msg};
+    } else {
+        result = Success{};
+    }
+
+    cass_future_free(cass_future);
+    cass_statement_free(cass_statement);
+
+    return result;
 }
